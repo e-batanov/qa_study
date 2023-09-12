9917e7b2b7e6aa681af436568e352fc9135e100a
e-batanov <evgen.batanov@yandex.ru>
2023-09-13 00:52:26 +0300

    Added fix_me.py file and fixed it with linter and .gitignore

---
+++ fix_me.py
@@ -0,0 +1,33 @@
+"""
+
+Скрипт для расчета среднего значения чисел в списке.
+"""
+
+
+def calculate_average(nums):
+    """
+    Рассчитывает среднее значение чисел в списке.
+
+    Args:
+        nums (list): Список чисел.
+
+    Returns:
+        float: Среднее значение чисел.
+    """
+    total = sum(nums)
+    count = len(nums)
+    average = total / count
+    return average
+
+
+def main():
+    """
+    Основная функция, которая выполняет расчет среднего значения списка и выводит результат.
+    """
+    nums = [10, 15, 20]
+    result = calculate_average(nums)
+    print("The average is:", result)
+
+
+if __name__ == "__main__":
+    main()

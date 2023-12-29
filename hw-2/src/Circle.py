098441089ec0677a2b0c2c8fcb706bad856be9f2
e-batanov <evgen.batanov@yandex.ru>
2023-12-29 12:03:24 +0300

    Add book_distribution.py and generate result

---
+++ hw-2/src/Circle.py
@@ -0,0 +1,14 @@
+from src.Figure import Figure
+from math import pi
+
+
+class Circle(Figure):
+    def __init__(self, radius):
+        super().__init__("Circle")
+        self.radius = radius
+
+    def area(self):
+        return pi * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * pi * self.radius
